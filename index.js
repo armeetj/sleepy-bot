474de0efbf4b9e9e294369952b29e743c5b9e86b
xarmeetx <xarmeetx@gmail.com>
2019-06-23 00:19:27 -0700

    fixed serverinfo.json system, adding servers, and deleting servers now functions as well as basic ping pong functions

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-//armeet
+//Armeet Singh Jatyani 2019
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var fs = require('fs');
 
+var serverInfo = '';
 //reading token
 fs.readFile('token.txt', 'utf8', (err, data) =>
 {
@@ -14,14 +16,13 @@ fs.readFile('token.txt', 'utf8', (err, data) =>
     {
         console.log(err.lineNumber);
     }
-
+    updateServerInfo();
 });
 
 
 //when bot is online, log to console
 client.on('ready', () =>
 {
-  try{
     console.log(`Logged in as ${client.user.tag}!`);
     console.log("Serving in "+ client.guilds.size + " guilds.\n");
     console.log("guilds: \n");
@@ -33,78 +34,67 @@ client.on('ready', () =>
         console.log("server: " + client.guilds.get(keyArray[i]).name + ", id: "+ keyArray[i]);
     }
     console.log("\n")
-  }catch(error)
-  {
-
-  }
 });
 
 
 //when client joins a new server
-
 client.on("guildCreate", (newGuild)=>
 {
-  try{
-    fs.readFile('serverinfo.json','utf8',(err, data)=>
-    {
-    	if(err)
-    	{
-    		 throw err;
-    		 console.log("ERROR!");
-    	}
-    		var oldData = JSON.parse(data);
-    		console.log(oldData.servers[0]);
-
-    		var newServer = {
-    			"id": newGuild.id,
-    			"name": newGuild.name,
-    			"prefix": ">",
-    			"channels": [],
-    		}
-
-    		oldData.servers.push(newServer)
-    		var newData = JSON.stringify(oldData);
-
-    		fs.writeFile("serverinfo.json", newData, (err)=>
-    		{
-    			if(err)
-    			{
-    				throw err;
-    				console.log("ERROR!");
-    			}
-    		});
-    });
-  }catch(e)
-  {
-
+  var newServer = {
+    "id": newGuild.id,
+    "name": newGuild.name,
+    "prefix": ">",
+    "channels": [],
   }
+
+  serverInfo.servers.push(newServer);
+  
+  var newData = JSON.stringify(serverInfo);
+
+  fs.writeFile("serverinfo.json", newData, (err)=>
+  {
+    if(err)
+    {
+      console.log("ERROR!");
+      throw err;
+    }
+  });
+  console.log("added server: " + newGuild.name + ", ID: " + newGuild.id);
 
 });
 
 //when client leaves the server, or the server was deleted
 client.on("guildDelete", (leftGuild)=>
 {
-  try
-  {
-    fs.readFile('serverinfo.json', 'utf8',(err, data)=>
-    {
-        var oldData = JSON.parse(data);
-        console.log("deleted guild: " + oldData.servers[oldData.servers.length-1]) + "\n";
-    });
-  }catch (e)
-  {
+  serverInfo.servers.splice(findServerInfoIndex(leftGuild.id), 1);
 
-  }
+  var newData = JSON.stringify(serverInfo);
+
+  fs.writeFile("serverinfo.json", newData, (err)=>
+  {
+    if(err)
+    {
+      console.log("ERROR!");
+      throw err;
+    }
+  });
+
+  console.log("deleted server: " + leftGuild.name + ", ID: " + leftGuild.id);
 
 });
 
 
-//when a message is recievesd
+//when a message is recieved
 client.on('message', msg =>
 {
   try{
-    console.log("message recieved from guild: "+ msg.guild.name + "\nid: " + msg.guild.id + "\n")
-    if (msg.content === 'ping') {
+    console.log("message recieved from guild: "+ msg.guild.name + "\nid: " + msg.guild.id)
+    
+    var serverPrefix = serverInfo.servers[findServerInfoIndex(msg.guild.id)].prefix;
+    console.log("serverPrefix: " + serverPrefix + "\n");
+  
+    if(msg.content === (serverPrefix + "ping"))
+    {
       msg.reply('Pong!');
     }
   }catch(e)
@@ -113,3 +103,25 @@ client.on('message', msg =>
   }
 
 });
+
+//given the guild id, returns the index in where in serverinfo.json
+//... that the serverinfo is located
+function findServerInfoIndex(guildID)
+{
+  for(i = 0; i < serverInfo.servers.length; i++)
+  {
+    if (guildID === serverInfo.servers[i].id)
+    {
+      return i;
+    }
+  }
+}
+
+//updates the serverInfo variable by reading serverinfo.json
+function updateServerInfo ()
+{
+  fs.readFile('serverinfo.json', 'utf8', (err, data) => 
+  {
+    serverInfo = JSON.parse(data);
+  })
+}
--- test.js
+++ test.js
@@ -1,34 +1,48 @@
 var fs = require('fs');
 
+// fs.readFile('serverinfo.json', 'utf8',(err, data)=>
+//     {
+//         var oldData = JSON.parse(data);
+// 		oldData.servers[0].id = "-1";
+//         // delete oldData.servers[0];
+//         var newData = JSON.stringify(oldData);
 
+//         fs.writeFile("serverinfo.json", newData, (err)=>
+//     		{
+//     			if(err)
+//     			{
+//             console.log("ERROR!");
+//     				throw err;
+//     			}
+//     		});
+//     });
 
+	fs.readFile('serverinfo.json','utf8',(err, data)=>
+    {
+    	if(err)
+    	{
+        console.log("ERROR!");
+    		throw err;
+    		
+    	}
+    		var oldData = JSON.parse(data);
+    		console.log(oldData.servers[0]);
 
-fs.readFile('serverinfo.json',(err, data)=>
-{
-	if(err)
-	{
-		 throw err;
-		 console.log("ERROR!");
-	}
-		var oldData = JSON.parse(data);
-		console.log(oldData.servers[0]);
+    		var newServer = {
+    			"id": newGuild.id,
+    			"name": newGuild.name,
+    			"prefix": ">",
+    			"channels": [],
+    		}
 
-		var newServer = {
-			"id": 586724485311234061,
-			"name": "new server",
-			"prefix": ">",
-			"channels": [],
-		}
+    		oldData.servers.push(newServer)
+    		var newData = JSON.stringify(oldData);
 
-		oldData.servers.push(newServer)
-		var newData = JSON.stringify(oldData);
-
-		fs.writeFile("serverinfo.json", newData, (err)=>
-		{
-			if(err)
-			{
-				throw err;
-				console.log("ERROR!");
-			}
-		});
-});
+    		fs.writeFile("serverinfo.json", newData, (err)=>
+    		{
+    			if(err)
+    			{
+            console.log("ERROR!");
+    				throw err;
+    			}
+        });
